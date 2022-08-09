e659f2240772e099398f977497bd51c219860709
hungvnhd <104383399+hungvnhd@users.noreply.github.com>
2022-08-10 02:41:40 +0700

    Add files via upload

---
+++ homeworks6.js
@@ -0,0 +1,113 @@
+// 1.
+
+// let a = Number(prompt("nhập số a"));
+// let b = Number(prompt("nhập số b"));
+// function sum2n(a, b) {
+//   result = a + b;
+//   alert(result);
+//   return result;
+// }
+// sum2n(a, b);
+
+//2.
+
+// array = [1, 2, 3, 4, 5, 6];
+// let a = 0;
+
+// function tongLe(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 != 0) {
+//       a = a + array[i];
+//     }
+//   }
+//   alert(a);
+//   return a;
+// }
+// tongLe(array);
+
+//bài 1:
+// let a = Number(prompt("enter number a"));
+// function sqr(a) {
+//   result = a * a;
+//   alert(`square of ${a} is ${result}`);
+//   return result;
+// }
+// sqr(a);
+
+//bài 2:
+// let r = Number(prompt("enter radius length of a circle"));
+// function perimeter(r) {
+//   result = 2 * Math.PI * r;
+//   alert(`perimeter of the circle is ${result}`);
+//   return result;
+// }
+// perimeter(r);
+
+// function area(r) {
+//   result = Math.PI * r * r;
+//   alert(`area of the circle is ${result}`);
+//   return result;
+// }
+// area(r);
+// bài 3:
+
+// let a = Number(prompt("enter number a"));
+// let b = 1;
+// function factorial(a) {
+//   for (let i = 1; i <= a; i++) {
+//     if (a > 0) {
+//       b = b * i;
+//     } else if (a === 0) {
+//       b = 1;
+//     } else if (a < 0) {
+//       b = "invalid number";
+//     }
+//   }
+//   alert(`factorial of ${a} is ${b}`);
+//   return b;
+// }
+// factorial(a);
+
+//bài 4:
+// let a = prompt("enter anything");
+// function checkingNumber(a) {
+//   return typeof a == "number";
+// }
+// console.log(checkingNumber(a));
+// checkingNumber(a);
+//bài 5
+
+// let a = Number(prompt("enter number a"));
+// let b = Number(prompt("enter number b"));
+// let c = Number(prompt("enter number c"));
+// let d;
+
+// function min(a, b, c) {
+//   d = Math.min(a, b, c);
+//   alert(d);
+//   return d;
+// }
+// min(a, b, c);
+
+//bài 6:
+
+// let a = Number(prompt("enter number a"));
+// function checkingNumber(a) {
+//   return a > 0 && a % 1 === 0;
+// }
+// console.log(checkingNumber(a));
+
+//bai7:
+// let a = Number(prompt("enter number a"));
+// let b = Number(prompt("enter number b"));
+// let c = 0;
+// function swap(a, b) {
+//   c = a;
+//   a = b;
+//   b = c;
+//   alert(`position after swaping: ${a} ${b}`);
+//   return a, b;
+// }
+// swap(a, b);
+
+//bai 8
